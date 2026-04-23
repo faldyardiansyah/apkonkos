f91d57963321ee44b40b24adae6c0ea93b90df6d
faldyardiansyah <faldyardiansyah3@gmail.com>
2026-04-23 15:48:24 +0700

    update appservice

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if ($this->app->environment('testing')) {
+        Vite::useHotFile(storage_path('framework/testing.hot'));
+    }
     }
 }
